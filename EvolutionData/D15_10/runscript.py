8a3ab2f3a283060ebcbdcc9cac32ee5df00f2a2b
Eduardo Izquierdo <edizquierdo@mail.com>
2021-07-16 08:34:08 -0400

    Updates

---
+++ EvolutionData/D15_10/runscript.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+fr = int(sys.argv[1])
+to = int(sys.argv[2])
+currentpath = os.getcwd()
+for k in range(fr,to):
+    print(k)
+    os.chdir(currentpath+'/'+str(k))
+    os.system('../main')
+    os.chdir('../')
